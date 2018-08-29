59089158e57626b2be1bfaca45b18cd1908238ee
Arnaldo <arnaldo.villarrubia@upr.edu>
2018-08-29 17:28:55 -0400

    Calcula el rank

--- src/JobHandler.java
+++ src/JobHandler.java
@@ -26,7 +26,39 @@ public class JobHandler {
 		
 		//Add Code Here [You have to use Switch and If/Else to get graded] 
 		
+		if(yearlySalary < 10000){
+			currentRank = 20;
+		}
+		
+		else if (yearlySalary > 10000 && yearlySalary < 50000){
+			currentRank = 40;
+		}
+		else if (yearlySalary > 50000 && yearlySalary < 100000){
+			currentRank = 60;
+		}
+		else if (yearlySalary >= 100000){
+			currentRank = 80;
+		}
+		
+		switch(warningTotals){
+		case 0:
+			break;
+		case 1:
+			currentRank = currentRank - 5;
+			break;
+		case 2:
+			currentRank = currentRank - 10;
+			break;
+		case 3:
+			currentRank = currentRank - 15;
+			break;	
+		default:
+			currentRank = currentRank - warningTotals*6;
+		}
+		
 		return currentRank - warningDeRank;
+		
+		
 	}
 	
 	/**
